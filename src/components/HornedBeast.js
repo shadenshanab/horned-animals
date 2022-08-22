67dffdb83fab9f1ee210d02986a5db236809c9a3
Shaden Shanab <shadenshanab2@gmail.com>
2022-08-22 21:07:07 +0300

    version2

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -3,7 +3,9 @@ import React from "react";
 class HornedBeast extends React.Component{
     render(){
         return(
+            
             <div>
+
                 <h2> {this.props.title} </h2>
                 <img src={this.props.image} alt='img'></img>
                 <p>{this.props.description}</p>
